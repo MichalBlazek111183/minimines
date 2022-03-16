ed0e417ec8de120f95621c3247a041b05f589e40
Michal <xblazekm1@stuba.sk>
2022-03-16 14:28:19 +0100

    tiles

---
+++ src/main/java/sk/stuba/fei/uim/oop/game/tiles/Empty.java
@@ -0,0 +1,9 @@
+package sk.stuba.fei.uim.oop.game.tiles;
+
+public class Empty extends Tile{
+
+    @Override
+    protected String drawRevealed() {
+        return ".";
+    }
+}
---
+++ src/main/java/sk/stuba/fei/uim/oop/game/tiles/Mine.java
@@ -0,0 +1,16 @@
+package sk.stuba.fei.uim.oop.game.tiles;
+
+public class Mine extends Tile{
+
+
+    @Override
+    protected String drawRevealed() {
+        return "*";
+    }
+
+    @Override
+    public void reveal() throws MineRevealedException {
+        super.reveal();
+        throw new MineRevealedException();
+    }
+}
